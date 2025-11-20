071cecd42aeaf8b483ad0b387dd0d79ce13f237d
David Barrett <39411621+David-Barrett-MS@users.noreply.github.com>
2025-11-20 13:11:57 +0000

    Create Create-OnlineMeeting.ps1

---
+++ Graph/Create-OnlineMeeting.ps1
@@ -0,0 +1,441 @@
+#
+# Create-OnlineMeeting.ps1
+#
+# By David Barrett, Microsoft Ltd. Use at your own risk.  No warranties are given.
+#
+#  DISCLAIMER:
+# THIS CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+<#
+.SYNOPSIS
+Creates an online meeting.
+
+.DESCRIPTION
+Script to test creating an online meeting for a user, using either delegated or application permissions.
+
+.EXAMPLE
+
+Delegate permissions (requires Mail.ReadWrite):
+.\Create-OnlineMeeting.ps1 -AppId $clientId -TenantId $tenantId
+
+Application permissions (requires Mail.ReadWrite):
+.\Create-OnlineMeeting.ps1 -mailbox $Mailbox -AppId $clientId -TenantId $tenantId -AppSecretKey $secretKey -TraceGraphCalls
+#>
+
+
+param (
+<# Auth.ps1 %PARAMS_START% #>
+    [Parameter(Mandatory=$False,HelpMessage="Application Id (obtained when registering the application in Azure AD).")]
+    [ValidateNotNullOrEmpty()]
+    [string]$AppId,
+
+    [Parameter(Mandatory=$False,HelpMessage="Application secret key (obtained when registering the application in Azure AD).  If not specified, delegate permissions are assumed.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$AppSecretKey,
+
+    [Parameter(Mandatory=$False,HelpMessage="Redirect Url (specified when registering the application in Azure AD, use localhost).  Required for delegate permissions.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$RedirectUrl = "http://localhost/code",
+
+    [Parameter(Mandatory=$False,HelpMessage="Tenant Id.  Default is common, which requires the application to be registered for multi-tenant use (and consented to in target tenant).")]
+    [ValidateNotNullOrEmpty()]
+    [string]$TenantId = "common",
+
+    [Parameter(Mandatory=$False,HelpMessage="Mailbox (me to access own mailbox when using delegate permissions).")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Mailbox = "me",
+<# Auth.ps1 %PARAMS_END% #>
+
+    [Parameter(Mandatory=$False,HelpMessage="Timeout for HTTP requests (default is 30 seconds, which is same as Microsoft Graph endpoint).  Only works for Powershell 7.4+ (prior to that, timeout is infinite).")]
+    [ValidateRange(0,300)]
+    [int]$ConnectionTimeout = 30,
+
+<# Logging.ps1 %PARAMS_START% #>
+    [Parameter(Mandatory=$False,HelpMessage="If included, activity is logged to a file (same as script name with .log appended).")]	
+    [switch]$LogToFile,
+<# Logging.ps1 %PARAMS_END% #>
+
+<# Graph.ps1 %PARAMS_START% #>
+    [Parameter(Mandatory=$False,HelpMessage="If specified, all Graph calls will be logged to file (same name as script, ending in .trace).")]
+    [switch]$TraceGraphCalls,
+<# Graph.ps1 %PARAMS_END% #>
+
+    [Parameter(Mandatory=$False,HelpMessage="Extended property definition to test.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$extendedPropertyDefinition = "String {00020329-0000-0000-C000-000000000046} Name cecp-49d3b812-abda-45b9-b478-9bc464ce5b9c",
+
+    [Parameter(Mandatory=$False,HelpMessage="If not null or empty, creates the extended property with this value on the first message in Inbox that doesn't already have it set.")]
+    [string]$CreatePropertyValue = "",
+
+    [Parameter(Mandatory=$False,HelpMessage="If not null or empty, filters for this specific value for the extended property.")]
+    [string]$FilterSpecificValue = ""
+)
+
+$script:ScriptVersion = "1.0.0"
+
+<# Logging.ps1 %FUNCTIONS_START% #>
+if ($LogToFile) {
+    $script:logFile = "$($MyInvocation.InvocationName).log"
+}
+
+Function LogToFile([string]$LogEntry)
+{
+	if ( [String]::IsNullOrEmpty($script:logFile) ) { return }
+	$LogEntry | Out-File $script:logFile -Append
+}
+
+Function Log([string]$Details, [System.ConsoleColor]$ForegroundColor = "Gray")
+{
+    $logEntry = "$([DateTime]::Now.ToShortDateString()) $([DateTime]::Now.ToLongTimeString())   $Details"
+    Write-Host $logEntry -ForegroundColor $ForegroundColor
+    LogToFile $logEntry
+}
+Log "$($MyInvocation.MyCommand.Name) version $($script:ScriptVersion) starting"
+<# Logging.ps1 %FUNCTIONS_END% #>
+
+<# Auth.ps1 %FUNCTIONS_START% #>
+function GetAppAuthToken()
+{
+    # Acquire token for application permissions
+    $body = @{grant_type="client_credentials";scope="https://graph.microsoft.com/.default";client_id=$AppId;client_secret=$AppSecretKey}
+    try
+    {
+        $script:oauth = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token -Body $body
+        $script:token_expires = (Get-Date).AddSeconds($oauth.expires_in)
+    }
+    catch
+    {
+        Write-Host "Failed to obtain OAuth token" -ForegroundColor Red
+        $Error[0]
+        exit # Failed to obtain a token
+    }
+    $script:graphBaseUrl = "https://graph.microsoft.com/v1.0/users/$Mailbox/"
+}
+
+function RenewOAuthToken()
+{
+    # If using app flow, we just obtain a new token
+
+    if (![String]::IsNullOrEmpty($AppSecretKey))
+    {
+        GetAppAuthToken
+        return
+    }
+
+    # If using delegate flow, we need to refresh the token
+    $body = @{grant_type="refresh_token";scope="https://graph.microsoft.com/.default";client_id=$AppId;refresh_token=$oauth.refresh_token}
+    try
+    {
+        $script:oauth = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token -Body $body
+        $script:token_expires = (Get-Date).AddSeconds($oauth.expires_in)
+    }
+    catch
+    {
+        Log "Failed to renew OAuth token" Red
+        exit # Failed to renew the token
+    }
+    Log "Successfully renewed OAuth token"
+}
+
+function UpdateHeaders()
+{
+    # Ensure the OAuth token is renewed if it is about to expire
+    if ($script:token_expires -lt (Get-Date).AddMinutes(5))
+    {
+        RenewOAuthToken
+    }
+
+    # Prepare the request headers
+    if ($null -eq $script:headers -or $script.headers.Count -eq 0)
+    {
+        $script:headers = @{
+            'Authorization'  = "$($oauth.token_type) $($oauth.access_token)";
+            'Content-Type'   = 'application/json';
+        }    
+    }
+}
+
+
+
+## Authentication
+
+if ([String]::IsNullOrEmpty($AppSecretKey))
+{
+    # Acquire auth code (needed to request token)
+    $authUrl = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/authorize?client_id=$AppId&response_type=code&redirect_uri=$RedirectUrl&response_mode=query&scope=openid%20profile%20email%20offline_access%20"
+    Start-Process $authUrl
+
+    Write-Host "Please complete log-in via the web browser, and then paste the redirect URL (including auth code) here to continue" -ForegroundColor Green
+    $authcode = Read-Host "Auth code"
+    $codeStart = $authcode.IndexOf("?code=")
+    if ($codeStart -gt 0)
+    {
+        $authcode = $authcode.Substring($codeStart+6)
+    }
+    $codeEnd = $authcode.IndexOf("&session_state=")
+    if ($codeEnd -gt 0)
+    {
+        $authcode = $authcode.Substring(0, $codeEnd)
+    }
+    Write-Verbose "Using auth code: $authcode"
+
+    # Acquire token (using the auth code)
+    $body = @{grant_type="authorization_code";scope="https://graph.microsoft.com/.default";client_id=$AppId;code=$authcode;redirect_uri=$RedirectUrl}
+    try
+    {
+        $oauth = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token -Body $body
+    }
+    catch
+    {
+        Log "Failed to obtain OAuth token" Red
+        $Error[0]
+        exit # Failed to obtain a token
+    }
+    $graphBaseUrl = "https://graph.microsoft.com/v1.0/me/"
+}
+else
+{
+    GetAppAuthToken
+}
+Log "Successfully obtained OAuth token"
+<# Auth.ps1 %FUNCTIONS_END% #>
+
+<# Graph.ps1 %FUNCTIONS_START% #>
+
+$script:traceFile = "$($MyInvocation.InvocationName).trace"
+function TraceInvokeRestMethod($method, $url, $body)
+{
+    UpdateHeaders
+    Write-Verbose "Calling URL: $url"
+    if ($TraceGraphCalls)
+    {
+        Add-Content -Path $script:traceFile -Value "$method $url"
+    }
+    try
+    {
+        $result = $null
+        $invokeParameters = @{
+            Method = $method
+            Uri = $url
+            Headers = $script:headers
+            ContentType = "application/json"
+        }
+        if ($PSVersionTable.PSVersion -ge [version]7.4)
+        {
+            # PowerShell 7.4+ supports ConnectionTimeoutSeconds
+            $invokeParameters.ConnectionTimeoutSeconds = $ConnectionTimeout
+        }
+
+        if ($method -eq "POST" -or $method -eq "PATCH")
+        {            
+            if ($TraceGraphCalls)
+            {
+                "`r`n$body" | out-file $script:traceFile -Append
+            }
+            $invokeParameters.Body = $body
+        }
+        $result = Invoke-WebRequest @invokeParameters -ErrorAction Stop
+        if ($TraceGraphCalls)
+        {
+            "" | out-file $script:traceFile -Append
+            $result.RawContent | out-file $script:traceFile -Append
+            "`r`n" | out-file $script:traceFile -Append
+        }
+    }
+    catch
+    {
+        if ($TraceGraphCalls)
+        {
+            # Check if there is a result object
+            if ($result) {
+                # If the result contains raw content, log it to the trace file
+                if ($result.RawContent) {
+                    "" | out-file $script:traceFile -Append
+                    $result.RawContent | out-file $script:traceFile -Append
+                    "`r`n" | out-file $script:traceFile -Append
+                }
+                else {
+                    # If raw content is not available, log the entire result object
+                    $result | out-file $script:traceFile -Append
+                }                
+            }
+            else {
+                # If no result object is available, check for exception response details
+                if ($Error[0].Exception.Response)
+                {
+                    # Extract and log response headers if available
+                    $headers = $Error[0].Exception.Response.Headers
+                    if ($headers -and $headers.Count -gt 0) {
+                        for ($i=0; $i -lt $headers.Count; $i++)
+                        {
+                            "$($headers.Keys[$i]): $($headers[$i])" | out-file $script:traceFile -Append
+                        }
+                        "" | out-file $script:traceFile -Append
+                    }
+                    else {
+                        # Log a message if no response headers are returned
+                        Log "No response headers returned" Red
+                    }
+                }
+                else {
+                    # Log a message if no further error details are available
+                    Log "No further error details available" Red
+                }                
+            }
+            # Log the error object to the trace file
+            $Error[0] | out-file $script:traceFile -Append
+        }
+        Log "$method failed to URL: $url"Red
+        return $null
+    }
+    return $result.Content
+}
+
+function GET($url)
+{
+    UpdateHeaders
+    Write-Verbose "Calling URL: $url"
+    try
+    {
+        $result = TraceInvokeRestMethod "GET" $url
+    }
+    catch
+    {
+        Write-Host "GET failed from URL: $url" -ForegroundColor Red
+        return $null
+    }
+    return $result
+}
+
+function PATCH($url, $body)
+{
+    UpdateHeaders
+    Write-Verbose "Calling URL: $url"
+    try
+    {
+        $result = TraceInvokeRestMethod "PATCH" $url $body
+    }
+    catch
+    {
+        Write-Host "PATCH failed at URL: $url" -ForegroundColor Red
+    }
+    return $result
+}
+
+function DELETE($url)
+{
+    UpdateHeaders
+    Write-Verbose "Calling URL: $url"
+    try
+    {
+        $result = TraceInvokeRestMethod "DELETE" $url
+    }
+    catch
+    {
+        Write-Host "DELETE failed at URL: $url" -ForegroundColor Red
+    }
+    return $result
+}
+
+function rawGET($url)
+{
+    return TraceInvokeRestMethod "GET" $url $null
+}
+
+function GetMailboxFolder($queryString)
+{
+    $url = $script:graphBaseUrl + "mailFolders$queryString"
+    $response = GET $url
+    if ($null -eq $response)
+    {
+        return $null
+    }
+    return ConvertFrom-Json ($response)
+}
+
+function GetFolderMessages($folderId, $queryString)
+{
+    $url = $script:graphBaseUrl + "mailFolders/$folderId/messages$queryString"
+    $response = GET $url
+    if ($null -eq $response)
+    {
+        return $null
+    }
+    return ConvertFrom-Json ($response)
+}
+
+function GetMessages($queryString)
+{
+    $url = $script:graphBaseUrl + "messages$queryString"
+    $response = GET $url
+    if ($null -eq $response)
+    {
+        return $null
+    }    
+    return ConvertFrom-Json ($response)
+}
+
+function GetFolderMessageById($folderId, $messageId, $queryString)
+{
+    $url = $script:graphBaseUrl + "mailFolders/$folderId/messages/$messageId$queryString"
+    $response = GET $url
+    if ($null -eq $response)
+    {
+        return $null
+    }    
+    return ConvertFrom-Json ($response)
+}
+
+function GetFolderMessageByIdAttachments($folderId, $messageId)
+{
+    $url = $script:graphBaseUrl + "mailFolders/$folderId/messages/$messageId/attachments"
+    $response = GET $url
+    if ($null -eq $response)
+    {
+        return $null
+    }    
+    return ConvertFrom-Json ($response)
+}
+
+function DeleteFolderMessageById($folderId, $messageId)
+{
+    $url = $script:graphBaseUrl + "mailFolders/$folderId/messages/$messageId"
+    $response = DELETE $url
+    if ($null -eq $response)
+    {
+        return $null
+    }    
+    return $response
+}
+
+<# Graph.ps1 %FUNCTIONS_END% #>
+
+
+function CreateOnlineMeeting()
+{
+    $url = $script:graphBaseUrl + "onlineMeetings"
+    $body = @{
+        startDateTime = (Get-Date).AddMinutes(15).ToString("o")
+        endDateTime = (Get-Date).AddHours(1).AddMinutes(15).ToString("o")
+        subject = "Test Meeting created at $([DateTime]::Now.ToString("yyyy-MM-dd HH:mm:ss"))"
+    } | ConvertTo-Json -Depth 3
+
+    $response = TraceInvokeRestMethod "POST" $url $body
+    if ($null -eq $response)
+    {
+        Log "Failed to create online meeting" Red
+        return $null
+    }
+    $meeting = ConvertFrom-Json ($response)
+    Log "Successfully created online meeting with Join URL: $($meeting.joinWebUrl)" Green
+    return $meeting
+}
+
+# Create the online meeting
+CreateOnlineMeeting
